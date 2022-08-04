8550893f288641f8cd233c3db7a297cc063cf5eb
frankfanyq <fyq990508@sjtu.edu.cn>
2022-08-03 22:05:25 -0400

    make home page responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,28 @@ import React from "react"
 export default function Home() {
   return (
     <div className="container">
-      <div className="flex items-center space-x-20">
+      <div className="
+        flex flex-col items-center space-y-5
+        sm:flex-row sm:items-center sm:space-x-20 sm:space-y-0
+      ">
 
         <div>
-          <h1 className="text-5xl font-bold mb-4 text-white-1">
+          <h1 className="
+            font-bold mb-4 text-white-1 text-3xl
+            sm:text-5xl
+          ">
             Hi, I'm Yiqing Fan.
           </h1>
-          <p className="text-2xl text-white-2 mb-4">
-            I'm a Computer Science student at Columbia University. I build things to make my life easier.
+          <p className="
+            text-white-2 mb-4 text-xl
+            sm:text-2xl
+           ">
+            A software engineer who builds things to make my life easier.
           </p>
-          <p className="text-2xl text-white-2">
+          <p className="
+            text-white-2 text-xl
+             sm:text-2xl
+          ">
             Welcome to my personal website!
           </p>
         </div>

7fce69906a073006d56c82d422757d86f6cca8f4
yiqingfann <yf2602@columbia.edu>
2023-05-21 16:49:19 -0700

    feat: update home page intro and image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,42 @@
 import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
-import Layout from '../components/Layout';
-import Seo from '../components/Seo';
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
 
 // markdown code block styles
-import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
-deckDeckGoHighlightElement();
+import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader"
+deckDeckGoHighlightElement()
 
 export default function Home() {
   return (
     <>
       <Seo title="Yiqing Fan" />
       <Layout>
-        <div className="
-        flex flex-col space-y-5 px-align-clickable
-        md:flex-row md:items-center md:space-x-20 md:space-y-0
-      ">
-
+        <div
+          className="
+            flex flex-col space-y-5 px-align-clickable
+            md:flex-row md:items-center md:space-x-20 md:space-y-0
+          "
+        >
           <div>
-            <h1 className="page-title">
-              Hi, I'm Yiqing Fan.
-            </h1>
+            <h1 className="page-title">Hi, I'm Yiqing Fan.</h1>
             <p className="page-subtitle mt-4">
-              A software engineer who turns ideas into products that solve people's pain points.
+              I solve problems and make things happen =v=.
             </p>
             <p className="page-subtitle mt-4">
-              Have fun and make impact!
+              Using full-stack web development and AI models to solve people's
+              pain points. Have fun and make impact!
             </p>
           </div>
 
           <div className="flex justify-center">
-            <StaticImage src="../images/yiqingfan.jpg" className="w-64 rounded-lg" />
+            <StaticImage
+              src="../images/shimarin.png"
+              className="w-64 rounded-lg"
+            />
           </div>
-
         </div>
       </Layout>
     </>
-
   )
 }
